12eaac677ba552d9887234d3049c2823590094c7
Davide <davidegorbani@gmail.com>
2024-08-22 10:03:07 +0200

    added configuration files for zenoh

--- pub.cxx
+++ pub.cxx
@@ -1,50 +1,77 @@
 #include "zenoh.hxx"
+#include <ConfigFolderPath.h>
 #include <atomic>
 #include <chrono>
-#include <thread>
 #include <iostream>
+#include <thread>
 
 std::atomic<bool> isClosing{false};
 
-int main(int argc, char** argv)
-{
-	std::cerr << "pub.exe: " << std::endl;
-    double periodInSec = 0.01;
+int main(int argc, char **argv) {
+  std::cerr << "pub.exe: " << std::endl;
+  double periodInSec = 0.017;
 
-    zenoh::Config config;
-    auto session = zenoh::expect<zenoh::Session>(open(std::move(config)));
-    int periodInUs = periodInSec * 1e6;
-    auto Ts = std::chrono::microseconds(periodInUs);
+  zenoh::Config config = zenoh::expect(zenohc::config_from_file(
+      (getConfigPath() + "/zenohConfigPub.json5").c_str()));
+  auto session = zenoh::expect<zenoh::Session>(zenoh::open(std::move(config)));
+  int periodInUs = periodInSec * 1e6;
+  auto Ts = std::chrono::microseconds(periodInUs);
+  std::vector<zenohc::Publisher> publishersNodes;
+  std::vector<zenohc::Publisher> publishersShoes;
+  std::vector<int> nodes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  std::vector<int> shoes = {1, 2};
 
-    while (!isClosing)
-    {
-        auto startTime = std::chrono::high_resolution_clock::now();
+  for (auto i : nodes) {
+    session.declare_publisher("iFeel/node" + std::to_string(i) + "/linAcc");
+    session.declare_publisher("iFeel/node" + std::to_string(i) + "/angVel");
+    session.declare_publisher("iFeel/node" + std::to_string(i) +
+                              "/orientation");
+    publishersNodes.emplace_back(
+        zenohc::expect<zenohc::Publisher>(session.declare_publisher(
+            "iFeel/node" + std::to_string(i) + "/linAcc")));
+    publishersNodes.emplace_back(
+        zenohc::expect<zenohc::Publisher>(session.declare_publisher(
+            "iFeel/node" + std::to_string(i) + "/angVel")));
+    publishersNodes.emplace_back(
+        zenohc::expect<zenohc::Publisher>(session.declare_publisher(
+            "iFeel/node" + std::to_string(i) + "/orientation")));
+  }
 
-        std::vector<double> accNode = {1.0, 2.0, 3.0};
-        std::vector<double> angVelNode = {0.0, 0.0, 0.0};
-        std::vector<double> rotNode = {1.0, 0.0, 0.0, 0.0};
+  for (auto i : shoes) {
+    session.declare_publisher("iFeel/shoe" + std::to_string(i) + "/FT");
+    publishersShoes.emplace_back(zenohc::expect<zenohc::Publisher>(
+        session.declare_publisher("iFeel/shoe" + std::to_string(i) + "/FT")));
+  }
 
-        for (int i = 1; i <= 1; i++)
-        {
-            session.put("dummyData/IMU" + std::to_string(i) + "/linAcc", accNode);
-            /*
-            session.put("dummyData/IMU" + std::to_string(i) + "/angVel", angVelNode);
-            session.put("dummyData/IMU" + std::to_string(i) + "/orientation", rotNode);
-            session.put("dummyData/IMU" + std::to_string(i) + "/magnetometer", rotNode);*/
-        }
+  while (!isClosing) {
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-        auto endTime = std::chrono::high_resolution_clock::now();
+    std::vector<double> accNode = {1.0, 2.0, 3.0};
+    std::vector<double> angVelNode = {0.0, 0.0, 0.0};
+    std::vector<double> rotNode = {1.0, 0.0, 0.0, 0.0};
+    std::vector<double> FTShoe = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
 
-        auto nextTime = startTime + Ts;
-
-        auto remaining_ms
-            = std::chrono::duration_cast<std::chrono::milliseconds>(nextTime - endTime);
-
-        if (remaining_ms.count() > 0)
-        {
-            std::this_thread::sleep_until(nextTime);
-        }
+    for (int i = 1; i <= nodes.size(); i++) {
+      publishersNodes[3 * i - 3].put(accNode);
+      publishersNodes[3 * i - 2].put(angVelNode);
+      publishersNodes[3 * i - 1].put(rotNode);
     }
 
-    return 0;
+    for (int i = 1; i <= shoes.size(); i++) {
+      publishersShoes[i - 1].put(FTShoe);
+    }
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    auto nextTime = startTime + Ts;
+
+    auto remaining_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+        nextTime - endTime);
+
+    if (remaining_ms.count() > 0) {
+      std::this_thread::sleep_until(nextTime);
+    }
+  }
+
+  return 0;
 }
--- sub.cxx
+++ sub.cxx
@@ -1,14 +1,15 @@
 #include "zenoh.hxx"
 
+#include "spdlog/spdlog.h"
+#include <ConfigFolderPath.h>
 #include <algorithm>
 #include <atomic>
+#include <boost/format.hpp> // only needed for printing
+#include <boost/histogram.hpp>
 #include <chrono>
 #include <numeric>
-#include <thread>
-#include "spdlog/spdlog.h"
-#include <boost/format.hpp>    // only needed for printing
-#include <boost/histogram.hpp>
 #include <sstream>
+#include <thread>
 
 std::atomic<bool> isClosing = false;
 std::chrono::high_resolution_clock::time_point previousTime;
@@ -17,108 +18,123 @@ std::atomic<bool> firstRun = true;
 std::vector<double> g_allTimeMicroseconds;
 std::vector<double> g_TimeErrorMicroseconds;
 
-void printVectorStats(const std::vector<double>& inputData)
-{
-    std::vector<double> localDataCopy = inputData;
+void printVectorStats(const std::vector<double> &inputData) {
+  std::vector<double> localDataCopy = inputData;
 
-    // Sort the vector to calculate median and other statistics
-    std::sort(localDataCopy.begin(), localDataCopy.end());
+  // Sort the vector to calculate median and other statistics
+  std::sort(localDataCopy.begin(), localDataCopy.end());
 
-    // Calculate size
-    size_t size = localDataCopy.size();
+  // Calculate size
+  size_t size = localDataCopy.size();
 
-    // Calculate mean
-    double sum = std::accumulate(localDataCopy.begin(), localDataCopy.end(), 0.0);
-    double mean = sum / size;
+  // Calculate mean
+  double sum = std::accumulate(localDataCopy.begin(), localDataCopy.end(), 0.0);
+  double mean = sum / size;
 
-    // Calculate median
-    double median;
-    if (size % 2 == 0) {
-        median = (localDataCopy[size / 2 - 1] + localDataCopy[size / 2]) / 2;
-    } else {
-        median = localDataCopy[size / 2];
-    }
+  // Calculate median
+  double median;
+  if (size % 2 == 0) {
+    median = (localDataCopy[size / 2 - 1] + localDataCopy[size / 2]) / 2;
+  } else {
+    median = localDataCopy[size / 2];
+  }
 
-    // Calculate min and max
-    double min = localDataCopy.front();
-    double max = localDataCopy.back();
+  // Calculate min and max
+  double min = localDataCopy.front();
+  double max = localDataCopy.back();
 
-    // Print the results
-    double millisecondsInMicroseconds = 1000.0;
-    spdlog::info("Size: {} samples", size);
-    spdlog::info("Mean: {} ms", mean/millisecondsInMicroseconds);
-    spdlog::info("Median: {} ms", median/millisecondsInMicroseconds);
-    spdlog::info("Min: {} ms", min/millisecondsInMicroseconds);
-    spdlog::info("Max: {} ms", max/millisecondsInMicroseconds);
+  // Print the results
+  double millisecondsInMicroseconds = 1000.0;
+  spdlog::info("Size: {} samples", size);
+  spdlog::info("Mean: {} ms", mean / millisecondsInMicroseconds);
+  spdlog::info("Median: {} ms", median / millisecondsInMicroseconds);
+  spdlog::info("Min: {} ms", min / millisecondsInMicroseconds);
+  spdlog::info("Max: {} ms", max / millisecondsInMicroseconds);
 
-    // Create a histogram using Boost.Histogram
-    auto hist = boost::histogram::make_histogram(
-        boost::histogram::axis::regular<>(20, 0, 100.0*millisecondsInMicroseconds)); // 10 bins from 0 to 100 ms
+  // Create a histogram using Boost.Histogram
+  auto hist =
+      boost::histogram::make_histogram(boost::histogram::axis::regular<>(
+          20, 0,
+          15.0 * millisecondsInMicroseconds)); // 10 bins from 0 to 100 ms
 
-    // Fill the histogram with data
-    for (const auto& value : localDataCopy) {
-        hist(value);
-    }
+  // Fill the histogram with data
+  for (const auto &value : localDataCopy) {
+    hist(value);
+  }
 
-    std::ostringstream os;
-    for (auto&& x : boost::histogram::indexed(hist, boost::histogram::coverage::all)) {
-        os << boost::format("bin %2i [%4.3f, %4.3f): %i\n")
-            % x.index() % (x.bin().lower()/millisecondsInMicroseconds) % (x.bin().upper()/millisecondsInMicroseconds) % *x;
-    }
+  std::ostringstream os;
+  for (auto &&x :
+       boost::histogram::indexed(hist, boost::histogram::coverage::all)) {
+    os << boost::format("bin %2i [%4.3f, %4.3f): %i\n") % x.index() %
+              (x.bin().lower() / millisecondsInMicroseconds) %
+              (x.bin().upper() / millisecondsInMicroseconds) % *x;
+  }
 
-    spdlog::info("Histogram:\n {}", os.str());
+  spdlog::info("Histogram:\n {}", os.str());
 
-    return;
+  return;
 }
 
-void data_handler(const zenoh::Sample& sample)
-{
-    std::string_view keyExprStr(sample.get_keyexpr().as_string_view());
+void data_handler(const zenoh::Sample &sample) {
+  std::string_view keyExprStr(sample.get_keyexpr().as_string_view());
 
-    if (keyExprStr == "dummyData/IMU1/linAcc")
-    {
-        auto now = std::chrono::high_resolution_clock::now();
-        if (!firstRun && !isClosing)
-        {
-            // This needs to be coherente with the value used in the publisher
-            double aPrioriPeriodInMicro = 0.01*1000000.0;
-            double timeElapsedFromLastRead = std::chrono::duration_cast<std::chrono::microseconds>(now - previousTime).count();
-		    g_allTimeMicroseconds.push_back(timeElapsedFromLastRead);
-            g_TimeErrorMicroseconds.push_back(std::abs(timeElapsedFromLastRead-aPrioriPeriodInMicro));
-        }
-        previousTime = now;
-        firstRun = false;
+  if (keyExprStr == "iFeel/node1/linAcc") {
+    auto now = std::chrono::high_resolution_clock::now();
+    if (!firstRun && !isClosing) {
+      // This needs to be coherente with the value used in the publisher
+      double aPrioriPeriodInMicro = 0.017 * 1000000.0;
+      double timeElapsedFromLastRead =
+          std::chrono::duration_cast<std::chrono::microseconds>(now -
+                                                                previousTime)
+              .count();
+      g_allTimeMicroseconds.push_back(timeElapsedFromLastRead);
+      g_TimeErrorMicroseconds.push_back(
+          std::abs(timeElapsedFromLastRead - aPrioriPeriodInMicro));
     }
-
+    previousTime = now;
+    firstRun = false;
+  }
 }
 
-int main()
-{
-    zenoh::Config config;
-    std::vector<std::string> jointsList;
-    auto session = zenoh::expect<zenoh::Session>(open(std::move(config)));
-    auto subscriber = zenoh::expect<zenoh::Subscriber>(
-        session.declare_subscriber("dummyData/**", data_handler));
+int main() {
+  zenoh::Config config = zenoh::expect(zenohc::config_from_file(
+      (getConfigPath() + "/zenohConfigSub.json5").c_str()));
+  std::vector<std::string> jointsList;
+  auto session = zenoh::expect<zenoh::Session>(open(std::move(config)));
+  std::vector<zenoh::Subscriber> subscribers;
+  std::vector<int> nodes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  std::vector<int> shoes = {1, 2};
+  for (auto i : nodes) {
+    subscribers.emplace_back(
+        zenoh::expect<zenoh::Subscriber>(session.declare_subscriber(
+            "iFeel/node" + std::to_string(i) + "/*", data_handler)));
+  }
+  for (auto i : shoes) {
+    subscribers.emplace_back(
+        zenoh::expect<zenoh::Subscriber>(session.declare_subscriber(
+            "iFeel/shoe" + std::to_string(i) + "/*", data_handler)));
+  }
+  //   auto subscriber = zenoh::expect<zenoh::Subscriber>(
+  //       session.declare_subscriber("iFeel/**", data_handler));
 
-    auto startProgram = std::chrono::high_resolution_clock::now();
-    while (!isClosing)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  auto startProgram = std::chrono::high_resolution_clock::now();
+  while (!isClosing) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        auto now = std::chrono::high_resolution_clock::now();
+    auto now = std::chrono::high_resolution_clock::now();
 
-        if (std::chrono::duration_cast<std::chrono::seconds>(now - startProgram).count() >= 5.0)
-        {
-            isClosing = true;
-        }
-
+    if (std::chrono::duration_cast<std::chrono::seconds>(now - startProgram)
+            .count() >= 10.0) {
+      isClosing = true;
     }
+  }
 
-    // Print stats
-    spdlog::info("=== Statistics for time between each read ===");
-    printVectorStats(g_allTimeMicroseconds);
-    spdlog::info("=== Statistics of error of time between each read and nominal period ===");
-    printVectorStats(g_TimeErrorMicroseconds);
+  // Print stats
+  spdlog::info("=== Statistics for time between each read ===");
+  printVectorStats(g_allTimeMicroseconds);
+  spdlog::info("=== Statistics of error of time between each read and nominal "
+               "period ===");
+  printVectorStats(g_TimeErrorMicroseconds);
 
-    return 0;
+  return 0;
 }
